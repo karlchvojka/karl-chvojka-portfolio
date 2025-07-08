cab469bdca4c61387f1f89e72183c7b723ab8337
karlchvojka <karl.chvojka@gmail.com>
2025-07-08 15:08:28 -0400

    FEAT: Font import setup

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,5 +1,14 @@
 'use client'
 
+import styles from  "./Header.module.css";
+import { Montserrat } from 'next/font/google';
+
+export const montserrat_init = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat',
+  weight: '300',
+});
 /**
  * Header
  *
@@ -9,7 +18,7 @@
  */
 export default function Header() {
   return (
-  <header>
+  <header className={styles.header, `${montserrat_init.variable}`}>     
       <p>Header test</p>
   </header>
   )
